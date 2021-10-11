f69001df948b26c30d5b2d8d680ba86e5a1b6e46
NguyenTranMinhTrungITITIU20332 <91869278+NguyenTranMinhTrungITITIU20332@users.noreply.github.com>
2021-10-11 15:10:55 +0700

    first try

--- src/Exercise3.c
+++ src/Exercise3.c
@@ -14,8 +14,52 @@ int main(int argc, char *argv[]) {
 	//testing variable, applying it to your algorithm for auto-evaluating
 	int testcase = atoi(argv[1]);
 	
-	//Your codes here
-	
-	
+	int a[1000];
+	int dem = 0;
+	int kt = 0;
+	for (int i = 2; i <= testcase - 1; i++)
+		if (testcase % i == 0)
+			kt = 1;
+	if (kt == 0)
+		printf("%d", testcase);
+	if (kt == 1)
+	{
+		for (int i = 2; i <= testcase - 1; i++)
+		{
+			if (i == 2)
+			{
+				a[dem] = i;
+				dem += 1;
+			}
+			else
+			{
+				int kt2 = 0;
+				for (int j = 2; j <= i - 1; j++)
+					if (i % j == 0)
+						kt2 = 1;
+				if (kt2 == 0)
+				{
+					dem += 1;
+					a[dem] = i;
+				}
+			}
+		}
+		int i = 0;
+		while (testcase != 1)
+		{
+			if (testcase == a[i])
+			{
+				testcase = 1;
+				printf("%d", a[i]);
+			}
+			else if (testcase % a[i] == 0)
+			{
+				testcase /= a[i];
+				printf("%d * ", a[i]);
+			}
+			else if (testcase % a[i] != 0)
+				i += 1;
+		}
+	}
 	return 0;
 }
